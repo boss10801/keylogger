6a94c950d1968fcedf92d195eec161ca942fafd6
boss10801 <boss101801@gmail.com>
2023-12-29 13:24:05 +0700

    Create a basic listner based on ref

--- keylogger.py
+++ keylogger.py
@@ -1,1 +1,18 @@
-#Testing
+#Install pynput
+from pynput.keyboard import Key, Listener
+
+# Reference https://pypi.org/project/pynput/
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+
+def on_release(key):
+    print('{0} released'.format(key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+with Listener(on_press = on_press ,on_release = on_release) as listener:
+    listener.join()
